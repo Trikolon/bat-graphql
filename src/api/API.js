74eecbe5d2590eda6d0475d92d5d931984baab0b
Trikolon <1077860+Trikolon@users.noreply.github.com>
2018-08-07 21:14:51 +0200

    Refactor for apollo-server compatibility  * Removed custom CORS code. Always allow '*'  * Removed obsolete dependencies    * Express    * Body-parser

--- src/api/API.js
+++ src/api/API.js
@@ -1,6 +1,4 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import { graphqlExpress, graphiqlExpress } from 'apollo-server-express';
+import { ApolloServer } from 'apollo-server';
 
 import schema from './schema';
 
@@ -22,52 +20,12 @@ export default class API {
     this.schema = schema(this.db);
 
     // Intialize webserver
-    this.app = express();
-
-    // CORS
-    this.app.use((...args) => this.corsHandler(...args));
-    // Register api route
-    this.app.use('/graphql', bodyParser.json(), graphqlExpress({ schema: this.schema }));
-    this.app.get('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }));
+    this.app = new ApolloServer({ schema: this.schema });
   }
 
   listen() {
     return new Promise((resolve) => {
       this.app.listen(this.port, this.hostname, () => resolve);
     });
-  }
-
-  /**
-   * Appends CORS headers to http replies
-   * Allowed origins can be set in the config file
-   * @param req - Client request
-   * @param res - Client response object
-   * @param next - Method to call next middleware /handler
-   * @returns {null}
-   */
-  corsHandler(req, res, next) {
-    const { allowedOrigins } = this.cors;
-    const clientOrigin = req.get('origin');
-
-    let originResult;
-    if (allowedOrigins.indexOf('*') !== -1) { // Config has *, allow all origins
-      originResult = '*';
-    } else {
-      const index = allowedOrigins.indexOf(clientOrigin);
-      if (index === -1) {
-        originResult = 'null';
-      } else {
-        originResult = allowedOrigins[index];
-      }
-    }
-
-    res.header('Access-Control-Allow-Origin', originResult);
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-
-    // If it's a pre-flight request reply to client and interrupt middleware chain
-    if (req.method === 'OPTIONS') {
-      return res.sendStatus(204);
-    }
-    return next();
   }
 }
